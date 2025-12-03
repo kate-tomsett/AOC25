6c633740f4ae1714a0dd69a6911295c6dc00d1cd
kate-tomsett <tomsettkate@gmail.com>
2025-12-03 22:31:19 +0000

    Realised i could use my part 2 function to do part 1 too (even if it does take a bit longer lol

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,29 +1,15 @@
 advent_of_code::solution!(3);
 
-fn get_max_values (battery_bank: Vec<u64>) -> u64{
-
-    // We get the index and the first maximum value from the battery bank (not including the last digit so we can use that as the second largest value)
-    let first_max_value = battery_bank[..battery_bank.len()-1].iter().max().unwrap();  // get the max of battery_bank(start:end-1)
-    let idx = battery_bank.iter().position(|x| x == first_max_value).unwrap(); // find the first index of this maximum
-
-    // We get the second max value which comes after the first max value in battery bank
-    let second_max_value = battery_bank[idx+1..battery_bank.len()].iter().max().unwrap();
-
-    // Return a u64 which combines the two max values we found
-    return first_max_value * 10 + second_max_value
-}
-
-
-fn greedy_largest_subsequence(battery_bank: Vec<u64>) -> u64 {
+fn greedy_largest_subsequence(battery_bank: Vec<u64>, length_to_keep: usize) -> u64 {
     // This is the greedy largest subsequence algorithm
-    // It takes a vector input
-    // It gives you the larges values of length k (12) by removing some digits and doesn't change the relative order of the values.
-    // Outputs a new 12 length vector
+    // It takes a vector input and how many (k) digits we want to keep
+    // It gives you the larges values of length k by removing some digits and doesn't change the relative order of the values.
+    // Outputs a new k length vector
     // A chat-gpt special right here
 
     // This is our output vector of the largest k-long number we can make with our input.
     let mut output_joltage: Vec<u64> = Vec::new();
-    let total_to_remove = battery_bank.len() - 12; // We want to retain 12 digits
+    let total_to_remove = battery_bank.len() - length_to_keep;// We want to retain k digits
     let mut removed = 0;
 
     // Loop over each original digit
@@ -44,9 +30,9 @@ fn greedy_largest_subsequence(battery_bank: Vec<u64>) -> u64 {
     }
 
     // If the values in the original vector were already in order, we would have an identical vector so truncate down to size.
-    output_joltage.truncate(12);
+    output_joltage.truncate(length_to_keep);
 
-    // This creates a 12 digit u64 value out of our new vector of values by looping over the vector, mutiplying each previous value by 10 and adding our new vector element.
+    // This creates a k digit u64 value out of our new vector of values by looping over the vector, mutiplying each previous value by 10 and adding our new vector element.
     output_joltage.iter().fold(0u64, |acc, &d| acc * 10 + d)
 }
 
@@ -70,8 +56,7 @@ pub fn part_one(input: &str) -> Option<u64> {
             .collect();
 
         // Get the two largest values to get the 2 digit output joltage for this battery bank. Add on to the total.
-        total_joltage_output += get_max_values(battery_bank_digits);
-
+        total_joltage_output += greedy_largest_subsequence(battery_bank_digits,2);
     }
 
     println!("Total joltage output: {total_joltage_output}");
@@ -99,7 +84,7 @@ pub fn part_two(input: &str) -> Option<u64> {
 
         // Get the largest 12 digit output voltage we can from this battery bank. Add on to the total.
         // Apparently this is called the greedy largest subsequence algorithm which is cool.
-        total_joltage_output += greedy_largest_subsequence(battery_bank_digits);
+        total_joltage_output += greedy_largest_subsequence(battery_bank_digits,12);
 
     }
 
